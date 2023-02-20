d2e94c6fd09ab3d207b1defb1c2562eb8a0712c2
neethu <neethu.test@email.com>
2023-02-20 11:18:03 +0530

    First commit by Neethu "OrangeHRM Login"

---
+++ src/pack/OrangeLogin.java
@@ -0,0 +1,40 @@
+package pack;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class OrangeLogin {
+	@Test
+	   public void launchbrowser() throws InterruptedException {
+
+	 System.setProperty("webdriver.gecko.driver","E:\\geckodriver-v0.29.1-win64\\geckodriver.exe");
+	WebDriver driver=new FirefoxDriver();
+
+	driver.get("http://localhost/orangehrm/orangehrm-5.2/orangehrm-5.2/web/index.php/auth/login");
+
+	Thread.sleep(5000);
+
+	driver.findElement(By.name("username")).sendKeys("Rachel");
+	driver.findElement(By.name("password")).sendKeys("Rachel@2023");
+
+	WebElement login=driver.findElement(By.xpath("//button[normalize-space()='Login']"));
+
+	login.click();
+
+	Thread.sleep(5000);
+
+	WebElement PIM=driver.findElement(By.xpath("//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name'][normalize-space()='PIM']"));
+
+	PIM.click();
+
+	Thread.sleep(5000);
+
+	WebElement addemployee=driver.findElement(By.xpath("//a[normalize-space()='Add Employee']"));
+
+	addemployee.click();
+
+	}
+}
